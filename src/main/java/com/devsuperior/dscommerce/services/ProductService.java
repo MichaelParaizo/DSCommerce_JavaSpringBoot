df7b52e8ea0d636967ebdce6afa62a2005c325a4
Michael <michael-couto@hotmail.com>
2024-07-07 19:26:29 -0300

    DELETE product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -47,6 +47,12 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public void delete(Long id){
+          repository.deleteById(id);
+
+    }
+
     private void copyDtotoEntity(ProductDTO dto, Product entity) {
 
         entity.setName(dto.getName());
