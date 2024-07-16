323d4f3dbd803ec55089e5a31303f0186838fc0d
Michael <michael-couto@hotmail.com>
2024-07-15 22:57:42 -0300

    Tratamento de exceção e validacao com Bean

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,13 +1,19 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
 
     private Long id;
+    @Size(min = 3,max = 80,message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10,message = "Descrição precisa ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
@@ -39,11 +45,13 @@ public class ProductDTO {
         return description;
     }
 
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
     public Double getPrice() {
         return price;
     }
 
-    public String getImgUrl() {
-        return imgUrl;
-    }
+
 }
